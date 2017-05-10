84e35f187f155a562f4814885df6ddeea79efd99
monomial <split.grade@gmail.com>
2017-05-10 14:26:33 -0700

    fixed a bug and reformatted the javascript
    fixed the bug where the computer never chose a new character.  Also reformatted the JavaScript and refactored some functionality into separate functions.  Also made some other minor changes here and there.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,68 @@
-var letters = ["a", "b", "c"];
-	var wins = 0;
-	var losses = 0;
-	var guesses_left = 8;
-	var guesses_made = [];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+var wins = 0;
+var losses = 0;
+var MAX_GUESSES = 8;
+var guesses_left = MAX_GUESSES;
+var guesses_made = [];
 
-	
+var computerPick;
 
-	var computerPick = letters[Math.floor(Math.random()*letters.length)];
+function computerThinks() {
+    computerPick = letters[Math.floor(Math.random() * letters.length)];
+}
 
-			console.log(computerPick);
+computerThinks(); // initial computer pick
 
+console.log(computerPick);
 
-	document.onkeyup = function(event) {
+function updateGameUI() {
+    var html = "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Possible Letters: " + letters + "</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guesses_left + "</p>" +
+        "<p>Guesses Made: " + guesses_made + "</p>";
 
-			var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
-			
-			console.log(playerGuess);
+    document.querySelector("#game").innerHTML = html;
+}
 
-			for (var i=0; i<guesses_made.length; i++) {
-				if (playerGuess === guesses_made[i]) {
-				alert("You've already guessed that.");
-				}
-			}
+function resetGame() {
+    guesses_left = MAX_GUESSES;
+    guesses_made = [];
+    computerThinks(); // computer picks a new letter
+}
 
+document.onkeyup = function (event) {
 
-			if (playerGuess === computerPick) {
-				wins++;
-				guesses_left = 8;
-				guesses_made = [];
-				alert("That's correct!");
-			}
+    var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-			if (playerGuess != computerPick) {
-				guesses_left -=1;
-				guesses_made.push(playerGuess);
-				console.log(guesses_made);
-			}
+    console.log(playerGuess);
 
-			if (guesses_left === 0) {
-				alert("Incorrect.  You lose.  The letter was " + computerPick + ".");
-				losses++;
-				guesses_left = 8;
-				guesses_made = [];
-				;
-			}
-
+    for (var i = 0; i < guesses_made.length; i++) {
+        if (playerGuess === guesses_made[i]) {
+            alert("You've already guessed that.");
+        }
+    }
 
 
+    if (playerGuess === computerPick) {
+        wins++;
+        alert("That's correct!  The correct letter was \"" + computerPick + "\".  It only took you " + (MAX_GUESSES - guesses_left + 1) + " tries.");
+        resetGame();
+    }
+    else {
+        guesses_left -= 1;
+        guesses_made.push(playerGuess);
+        console.log(guesses_made);
+    }
 
-		var html ="<p>Guess what letter I'm thinking of!</p>" + 
-		"<p>Wins: " + wins + "</p>" + 
-		"<p>Losses: " + losses + "</p>" +
-		"<p>Guesses Left: " + guesses_left + "</p>" +
-		"<p>Guesses Made: " + guesses_made + "</p>";
+    if (guesses_left === 0) {
+        alert("Incorrect.  You lose.  The letter was \"" + computerPick + "\"");
+        losses++;
+        resetGame();
+    }
 
-		document.querySelector("#game").innerHTML = html;
-		}
+    
+    updateGameUI();
+    
+}
